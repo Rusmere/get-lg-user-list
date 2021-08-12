3d724c9d242b11afac4aa1bc82d6d57cdaecae6c
Rusmere <gwynding88@outlook.com>
2021-08-12 12:23:57 +0800

    Update get-users.py

--- get-users.py
+++ get-users.py
@@ -6,5 +6,5 @@ with open('lg_user.txt','w+') as f:
                 h={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
                 r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h)
                 if(r.json()["users"]!=[None]):
+                        print(i)
                         f.write(r.text+'\n')
-                        time.sleep(1)
