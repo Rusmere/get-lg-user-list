300a0dfe448dcd0dca21e3a422fc36014a8c59f4
Rusmere <gwynding88@outlook.com>
2021-08-12 15:45:36 +0800

    Update get-users.py

--- get-users.py
+++ get-users.py
@@ -22,9 +22,5 @@ def savetofile(filename):
                         r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h)
                         if(r.json()["users"]!=[None]):
                                 f.write(r.text+'\n')
-def main():
-        for i in range(0,1000000,2000):
-                exec("thread%s=myThread(%d,'%s',%d),thread%s.start()"%(i,i,i,i,i))
-
-if __name__ == '__main__':
-    main()
+for i in range(0,1000000,2000):
+        exec("thread%s=myThread(%d,'%s',%d),thread%s.start()"%(i,i,i,i,i))
