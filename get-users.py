3cccebdda98706d0105b5bb8e4f9f44df0dde957
Rusmere <gwynding88@outlook.com>
2021-08-12 16:52:53 +0800

    Create get-users.py

---
+++ get-users.py
@@ -0,0 +1,10 @@
+import threading
+import time
+import requests
+with open("lg-users.txt",'w+') as f:
+  for i in range(0,1000000):
+        p={"keyword":i}
+        h={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
+        r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h)
+        if(r.json()["users"]!=[None]):
+                f.write(r.text+'\n')
