e43749bfab52ff5d0783df48244cb5945e3d440b
Rusmere <gwynding88@outlook.com>
2021-08-12 22:44:01 +0800

    Update get-users.py

--- get-users.py
+++ get-users.py
@@ -2,7 +2,7 @@ import threading
 import time
 import requests
 with open("lg-users.txt",'w+') as f:
-  for i in range(0,200):
+  for i in range(0,1000):
         p={"keyword":i}
         h={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
         r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h)
