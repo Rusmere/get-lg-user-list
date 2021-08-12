4d74e609caf0c53174670ddcd2cd7598f0fed8ae
Rusmere <gwynding88@outlook.com>
2021-08-12 15:10:32 +0800

    Update get-users.py

--- get-users.py
+++ get-users.py
@@ -15,7 +15,7 @@ class myThread (threading.Thread):
 
 def savetofile(filename):
         with open(filename,'w+') as f:
-                for i in range(int(filename),int()filename+2000):
+                for i in range(int(filename),int(filename)+2000):
                         p={"keyword":i}
                         h={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
                         r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h)
