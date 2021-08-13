0a368396948aae790e952a7c2f9a462d6dd6b944
Rusmere <gwynding88@outlook.com>
2021-08-13 14:58:22 +0800

    Update get-users.py

--- get-users.py
+++ get-users.py
@@ -1,15 +1,20 @@
 import threading
 import time
 import requests
-for i in range(0,1000,200):
-  for j in range(i-200,i):
-    exec("file%s=open('%s','w+')"%(i,i))
+start=time.time()
+for i in range(100,300,100):
+  for j in range(i-100,i):
+    if(j==i-100):
+      exec("file%s=open('%s','w+')"%(i,i))
     p={"keyword":j}
     h={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
     try:
-      r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h)
+      r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h,timeout=15)
       if(r.json()["users"]!=[None]):
-        print("UID:%s"%j)
-        exec(r"file%s.write(r.text),'\n'"%(i))
-    except TimeoutError:
-      exec("file%s.close()"%i)
+        print("Get UID:%s"%j)
+        exec(r"file%s.write(r.text+'\n')"%(i))
+        if(j==i):
+          exec("file%s.close()"%i)
+    except requests.exceptions.ConnectionError:
+      print("Connection Error.")
+print("Done,Time=%d"%float(time.time())-start)
