8d4248288fef41e23d8afb2bd87f50aa0e0cc37d
Rusmere <gwynding88@outlook.com>
2021-08-13 11:00:53 +0800

    Update get-users.py

--- get-users.py
+++ get-users.py
@@ -1,11 +1,14 @@
 import threading
 import time
 import requests
-with open("lg-users.txt",'w+') as f:
-  for i in range(0,1000):
-        p={"keyword":i}
-        h={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
-        r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h)
-        if(r.json()["users"]!=[None]):
-                print("UID:%s"%i)
-                f.write(r.text+'\n')
+for i in range(0,1000,200):
+  exec("file%s=open('%s','w+')"%(i,i))
+  p={"keyword":i}
+  h={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
+  try:
+    r=requests.get('https://www.luogu.com.cn/api/user/search',params=p,headers=h)
+    if(r.json()["users"]!=[None]):
+      print("UID:%s"%i)
+      exec("file%s.write(r.text+'\n')"%i)
+  except TimeoutError:
+    exec("file%s.close()"%i)
